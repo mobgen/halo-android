3b291144f47cb1b99c05eb1cee792da4e9e766f7
Fernando Souto <fernando.souto@mobgen.com>
2016-12-14 09:28:45 +0100

    [abstrtact-processor]: generate methods with JavaPoet from code annotations

--- sdk-libs/halo-content-annotations/src/main/java/com/mobgen/halo/android/content/annotations/HaloQuery.java
+++ sdk-libs/halo-content-annotations/src/main/java/com/mobgen/halo/android/content/annotations/HaloQuery.java
@@ -5,8 +5,8 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Target(ElementType.TYPE) @Retention(RetentionPolicy.SOURCE)
-public @interface HaloTable {
+@Target(ElementType.METHOD) @Retention(RetentionPolicy.SOURCE)
+public @interface HaloQuery {
     String name();
     String query();
 }
