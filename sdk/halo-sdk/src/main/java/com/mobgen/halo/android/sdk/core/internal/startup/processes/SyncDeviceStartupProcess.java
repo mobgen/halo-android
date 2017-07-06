d4886a42e7ecd474bcb30bc279003710d472b8eb
ficiverson <efexepe@gmail.com>
2017-07-06 15:46:33 +0200

    [sdk]: check connection to sync device

--- sdk/halo-sdk/src/main/java/com/mobgen/halo/android/sdk/core/internal/startup/processes/SyncDeviceStartupProcess.java
+++ sdk/halo-sdk/src/main/java/com/mobgen/halo/android/sdk/core/internal/startup/processes/SyncDeviceStartupProcess.java
@@ -2,6 +2,7 @@ package com.mobgen.halo.android.sdk.core.internal.startup.processes;
 
 import android.support.annotation.NonNull;
 
+import com.mobgen.halo.android.framework.common.utils.HaloUtils;
 import com.mobgen.halo.android.framework.toolbox.threading.Threading;
 import com.mobgen.halo.android.sdk.api.Halo;
 
@@ -17,6 +18,10 @@ public class SyncDeviceStartupProcess extends StartupProcess {
 
     @Override
     protected void onStart(@NonNull Halo halo) {
-        halo.getCore().manager().syncDevice().bypassHaloReadyCheck().threadPolicy(Threading.SAME_THREAD_POLICY).execute();
+        if(HaloUtils.isNetworkConnected(halo.context())) {
+            halo.getCore().manager().syncDevice().bypassHaloReadyCheck().threadPolicy(Threading.SAME_THREAD_POLICY).execute();
+        } else {
+            halo.getCore().manager().syncDeviceWhenNetworkAvailable(Threading.POOL_QUEUE_POLICY);
+        }
     }
 }
