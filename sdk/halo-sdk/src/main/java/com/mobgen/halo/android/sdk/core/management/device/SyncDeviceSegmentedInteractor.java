559f4453b3ea32345fd813bcfd42561e3459264b
Fernando Souto Gonzalez <fernando.souto@mobgen.com>
2017-07-18 16:56:03 +0200

    [sdk]: refresh notification token after change environment

--- sdk/halo-sdk/src/main/java/com/mobgen/halo/android/sdk/core/management/device/SyncDeviceSegmentedInteractor.java
+++ sdk/halo-sdk/src/main/java/com/mobgen/halo/android/sdk/core/management/device/SyncDeviceSegmentedInteractor.java
@@ -1,14 +1,18 @@
 package com.mobgen.halo.android.sdk.core.management.device;
 
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 
 import com.mobgen.halo.android.framework.common.exceptions.HaloParsingException;
 import com.mobgen.halo.android.framework.network.exceptions.HaloNetException;
+import com.mobgen.halo.android.framework.toolbox.bus.Event;
 import com.mobgen.halo.android.framework.toolbox.data.HaloResultV2;
 import com.mobgen.halo.android.framework.toolbox.data.HaloStatus;
 import com.mobgen.halo.android.sdk.core.HaloCore;
 import com.mobgen.halo.android.sdk.core.management.models.Device;
 import com.mobgen.halo.android.sdk.core.threading.HaloInteractorExecutor;
+
+import static com.mobgen.halo.android.sdk.core.management.HaloManagerApi.DEVICE_UPDATE_EVENT_ID;
 
 /**
  * Provides the device information synchronizing it when needed.
@@ -41,6 +45,10 @@ public class SyncDeviceSegmentedInteractor implements HaloInteractorExecutor.Int
         Device device = null;
         try {
             device = mDeviceRepository.syncDevice(mCore.segmentationTags());
+            //refresh notification token
+            Bundle params = new Bundle();
+            params.putParcelable("user", device);
+            mCore.framework().toolbox().eventHub().emit(new Event(DEVICE_UPDATE_EVENT_ID, params));
         } catch (HaloNetException | HaloParsingException e) {
             status.error(e);
         }
