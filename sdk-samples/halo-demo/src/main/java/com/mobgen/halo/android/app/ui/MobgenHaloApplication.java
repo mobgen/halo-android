86e20d9f02d885584226123a1aac654b09f7b087
fernandosouto <fernando.souto@mobgen.com>
2018-03-21 08:22:17 +0100

    [demo]: update service on boot

--- sdk-samples/halo-demo/src/main/java/com/mobgen/halo/android/app/ui/MobgenHaloApplication.java
+++ sdk-samples/halo-demo/src/main/java/com/mobgen/halo/android/app/ui/MobgenHaloApplication.java
@@ -210,7 +210,7 @@ public class MobgenHaloApplication extends HaloApplication {
         return installer
                 .debug(BuildConfig.DEBUG)
                 .printLogToFile(PrintLog.SINGLE_FILE_POLICY)
-                //.enableServiceOnBoot()
+                .enableServiceOnBoot()
                 .channelServiceNotification("My awesome name", R.drawable.myicon)
                 .enableDefaultTags(BuildConfig.BUILD_TYPE.equals("debug"))
                 .endProcesses(new ConfigurationModule());
