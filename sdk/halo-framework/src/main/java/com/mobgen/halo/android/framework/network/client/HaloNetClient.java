2efb1e978ca3a6a83c6ae80c7cf13104a4ce43ab
Kiko Bugía <enrique.otero.bugia@mobgen.com>
2018-07-04 14:56:25 +0200

    [new feature] enabling TLSv.1.1 and TLSv.1.2 on android <5.0

--- sdk/halo-framework/src/main/java/com/mobgen/halo/android/framework/network/client/HaloNetClient.java
+++ sdk/halo-framework/src/main/java/com/mobgen/halo/android/framework/network/client/HaloNetClient.java
@@ -15,6 +15,14 @@ import com.mobgen.halo.android.framework.network.exceptions.HaloNetParseExceptio
 import com.mobgen.halo.android.framework.network.exceptions.HaloNetworkExceptionResolver;
 
 import java.io.IOException;
+import java.security.KeyManagementException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.TrustManagerFactory;
+import javax.net.ssl.X509TrustManager;
 
 import okhttp3.CertificatePinner;
 import okhttp3.OkHttpClient;
@@ -71,6 +79,23 @@ public class HaloNetClient {
             //The certificate pinning SHA if available
             okBuilder.certificatePinner(pinner);
         }
+
+        try {
+            TrustManagerFactory trustManagerFactory = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
+            trustManagerFactory.init((KeyStore) null);
+            TrustManager[] trustManagers = trustManagerFactory.getTrustManagers();
+
+            if (trustManagers.length != 1 || !(trustManagers[0] instanceof X509TrustManager)) {
+                okBuilder.sslSocketFactory(new SSLSocketFactoryCompat(trustManagers));
+            } else {
+                okBuilder.sslSocketFactory(new SSLSocketFactoryCompat(trustManagers), (X509TrustManager) trustManagers[0]);
+            }
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (KeyStoreException e) {
+            e.printStackTrace();
+        }
+
         return okBuilder;
     }
 
